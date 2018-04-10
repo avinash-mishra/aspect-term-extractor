879f4e5f3418a67fb9ea0ef21e852586c78d70d5
avi <mishra@psinc.jp>
2018-04-10 19:47:26 +0900

    added pos tagger with noun,adjective,verb and adverb filtering

---
+++ preprocessing/pos_tagger.py
@@ -0,0 +1,30 @@
+import nltk
+
+
+class POSTagger:
+
+    @staticmethod
+    def pos_tagger(text):
+        text = nltk.word_tokenize(text)
+        tagged_pos_list = nltk.pos_tag(text)
+        print(tagged_pos_list)
+
+        return tagged_pos_list
+
+    def filter_pos_tag(self, tagged_text):
+        final_text_list = []
+        matching_tag = ['NN','NNS','NNP','NNPS','RB','RBR','RBS','JJ','JJR','JJS','VB','VBD','VBG','VBN','VBP','VBZ']
+
+        for word, tag in tagged_text:
+            final_text = []
+            if tag in matching_tag:
+                final_text.append(word)
+                final_text_list.append((' '.join(final_text)))
+        # print(final_text_list)
+        return final_text_list
+
+
+if __name__ == '__main__':
+    x = POSTagger()
+    k= x.pos_tagger("I am avinash and I am am Indian.")
+    x.filter_pos_tag(k)
