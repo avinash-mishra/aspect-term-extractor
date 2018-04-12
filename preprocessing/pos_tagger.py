7a0bc8e50afae7999b65a2a16d33dd60be075a89
avi <mishra@psinc.jp>
2018-04-12 18:06:06 +0900

    created string combine method

--- preprocessing/pos_tagger.py
+++ preprocessing/pos_tagger.py
@@ -2,6 +2,7 @@ import nltk
 from nltk.corpus import stopwords
 from string import punctuation
 
+
 class POSTagger:
 
     @staticmethod
@@ -11,12 +12,9 @@ class POSTagger:
         stopwords_en_withpunct = set(stopwords_en).union(set(punctuation))
         text = [word for word in text if word not in stopwords_en_withpunct]
         tagged_pos_list = nltk.pos_tag(text)
-        # print(tagged_pos_list)
-
         return tagged_pos_list
 
-    @staticmethod
-    def filter_pos_tag(tagged_text):
+    def filter_pos_tag(self, tagged_text):
         final_text_list = []
         matching_tag = ['NN','NNS','NNP','NNPS','RB','RBR','RBS','JJ','JJR','JJS','VB','VBD','VBG','VBN','VBP','VBZ']
 
@@ -25,11 +23,15 @@ class POSTagger:
             if tag in matching_tag:
                 final_text.append(word)
                 final_text_list.append((' '.join(final_text)))
-        # print(final_text_list)
-        return final_text_list
+        return self.combine(final_text_list)
+
+    @staticmethod
+    def combine(filtered_tags):
+        filtered_string = ' '.join(filtered_tags)
+        return filtered_string
 
 
 if __name__ == '__main__':
     x = POSTagger()
-    k= x.pos_tagger("I am avinash and I am an Indian.")
-    x.filter_pos_tag(k)
+    k = x.pos_tagger("still testing the battery life as i thought it would be better, but am very happy with the upgrade.")
+    y = x.filter_pos_tag(k)
